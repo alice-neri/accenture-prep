50dfe519501a31ddbf7a8b8ea9b13ca909ec80b1
Alice Neri <nikolasberd@gmail.com>
2026-09-23 19:40:41 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,33 @@
+print("Simple Calculator")
+
+def add(a, b): #addition
+    return a + b
+
+def sub(a, b): #subtraction
+    return a - b
+
+def mult(a, b): #multiplication
+    return a * b
+
+def div(a, b): #division
+    if b == 0:
+        return "Error: cannot divide by zero"
+    else:
+        return a / b
+
+a = float(input("Input a: "))
+b = float(input("Input b: "))
+act = input("Would you like to add, subtract, multiply, or divide (+, -, * or /)? ") #act = action
+
+if act == "+":
+    res = add(a, b)
+elif act == "-":
+    res = sub(a, b)
+elif act == "*":
+    res = mult(a, b)
+elif act == "/":
+    res = div(a, b)
+else:
+    res = "Invalid operation"
+
+print (res)
